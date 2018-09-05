bc48156f4ce0187d47cc4feda3a49844dbb8f609
Yakovlev Denis <barkov777@gmail.com>
2018-09-05 21:42:10 +0300

    Simple refactoring

--- static/public/js/main.js
+++ static/public/js/main.js
@@ -1,15 +1,31 @@
-var loader = "<img src='/images/loader.svg' class='loader'>",
-    loaderWhite = "<img src='/images/loader-white.svg' class='loader'>";
+"use strict";
 
-$(document).ready(function() { 
+var socket = io();
+var loader = "<img src='/images/loader.svg' class='loader'>";
+var loaderWhite = "<img src='/images/loader-white.svg' class='loader'>";
+var pathname = window.location.pathname;
 
-  var socket = io();
-  var pathname = window.location.pathname;
-  var loader = "<img src='/images/loader.svg' class='loader'>",
-      loaderWhite = "<img src='/images/loader-white.svg' class='loader'>";
-  
-  $("#req-submit").on("click", function(){
-    var prefDays = "";
+function sendConfirmationEmail(){
+  $.ajax({
+    type: 'post',
+    url: '/api/sendConfirmationEmail',
+    beforeSend: function(){
+      $('#status-message').html(loader);
+    },
+    error: function(response){
+      if (response.status == 500)
+        $('#status-message').html("Произошла ошибка! Попробуйте позже.");
+      else (response.status == 400)
+        $('#status-message').html("Email можно отправлять не чаще, чем раз в 15 минут.");
+    },
+    success: function(response){
+      $('#status-message').html("Ссылка с подтверждением была выслана на Ваш email.");
+    }
+  });
+}
+
+function sendBid() {
+  var prefDays = "";
     if ($('#monday')[0].checked) prefDays += "Пн ";
     if ($('#tuesday')[0].checked) prefDays += "Вт ";
     if ($('#wednesday')[0].checked) prefDays += "Ср ";
@@ -43,31 +59,11 @@ $(document).ready(function() {
         }
       }
     });
-  });
-
-  //Set active current li 
-  $('.nav > li > a[href="' + pathname + '"]').parent().addClass('active');
-
-});
-
-function sendConfirmationEmail(){
-  $.ajax({
-    type: 'post',
-    url: '/api/sendConfirmationEmail',
-    beforeSend: function(){
-      $('#status-message').html(loader);
-    },
-    error: function(response){
-      if (response.status == 500)
-        $('#status-message').html("Произошла ошибка! Попробуйте позже.");
-      else (response.status == 400)
-        $('#status-message').html("Email можно отправлять не чаще, чем раз в 15 минут.");
-    },
-    success: function(response){
-      $('#status-message').html("Ссылка с подтверждением была выслана на Ваш email.");
-    }
-  });
 }
+
+$(document).ready(function() { 
+  $('.nav > li > a[href="' + pathname + '"]').parent().addClass('active');
+});
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
