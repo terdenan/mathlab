e9cefb731031ca112913a23ba34572bafd82a756
Denis <denis@Alikhans-MacBook-Air.local>
2020-07-09 01:27:44 +0300

    Актуализация окружения

--- libs/telegram-bot.js
+++ libs/telegram-bot.js
@@ -1,7 +1,8 @@
 const config = require('config');
-const isProduction = process.env.NODE_ENV === 'production';
 const TelegramBot = require('node-telegram-bot-api');
-const bot = isProduction ? new TelegramBot(config.telegram.token, {polling: true}) : {};
+
+const bot = isProduction ? new TelegramBot(config.telegram.token, { polling: true }) : {};
+const isProduction = process.env.NODE_ENV === 'production';
 
 bot._sendMessage = (chatId, resp) => {
     return new Promise((resolve, reject) => {
